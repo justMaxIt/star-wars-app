ad0d94beaac7f4efba46a373dbf91aeac1a27b40
justMaxIt <maximfedor2015@gmail.com>
2020-09-07 16:34:54 +0300

    half lesson

--- src/Elements/MainContent.js
+++ src/Elements/MainContent.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const api = "https://swapi.dev/api/";
 function MainContent() {
-  return <>Main Content</>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!data) {
+      fetch(`${api}starships/9/`)
+        .then((res) => res.json())
+        .then((res) => setData(res))
+        .catch((error) => console.error(error));
+    }
+  });
+  return (
+    <>
+      Name: {data && data.name}
+      <br /> <br />
+      Model: {data && data.model}
+      <br /> <br />
+      Manufacturer: {data && data.manufacturer}
+      <br /> <br />
+      Starship class: {data && data.starship_class}
+      <br /> <br />
+    </>
+  );
 }
 
 export default MainContent;
--- src/Elements/Menu.js
+++ src/Elements/Menu.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Menu() {
-  return <>Menu</>;
+  let location = useLocation();
+
+  return (
+    <div className="menu">
+      <Link className={location.pathname === "/" ? "active" : ""} to="/">
+        Main
+      </Link>
+      <Link
+        className={location.pathname === "/persons" ? "active" : ""}
+        to="/persons"
+      >
+        Persons
+      </Link>
+    </div>
+  );
 }
 
 export default Menu;
