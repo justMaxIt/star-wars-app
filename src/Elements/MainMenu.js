b21c9b7811cb0653ec389b9b307911449c5f4b3e
justMaxIt <maximfedor2015@gmail.com>
2020-09-08 19:16:12 +0300

    add button ship search

--- src/Elements/MainMenu.js
+++ src/Elements/MainMenu.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function MainMenu(props) {
+  const [input, setInput] = useState(0);
   let { setShipNumber } = props;
+
   return (
     <div className="main-menu">
       <input
         type="number"
         placeholder="search"
-        onChange={(e) => setShipNumber(e.target.value)}
+        onChange={(e) => setInput(e.target.value)}
       />
+      <button onClick={() => setShipNumber(input)}> Ship information </button>
     </div>
   );
 }
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -3,11 +3,11 @@ import MainContent from "../Elements/MainContent";
 import MainMenu from "../Elements/MainMenu";
 
 function MainPage() {
-  const [shipNumber, setShipNumber] = useState(9);
+  const [shipNumber, setShipNumber] = useState(null);
 
   return (
     <div className="main-page">
-      <MainMenu shipNumber={shipNumber} setShipNumber={setShipNumber} />
+      <MainMenu setShipNumber={setShipNumber} />
       <MainContent shipNumber={shipNumber} />
     </div>
   );
