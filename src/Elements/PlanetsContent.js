cde472eb5377089d2e1f07f494981e63a085c2a9
justMaxIt <maximfedor2015@gmail.com>
2020-09-09 15:43:40 +0300

    every second planet

--- src/Elements/PlanetsContent.js
+++ src/Elements/PlanetsContent.js
@@ -11,24 +11,21 @@ function PlanetsContent(props) {
       .then((res) => setData(res.results))
       .catch((error) => console.error(error));
   }, []);
-  console.log(data);
-  // for (let i = 0; i <= data.length; i++) {
-  //   if (i%2 != 1){setData(data[])}
 
   return (
     <>
-
-      {/* if (index%2 != 1){ */}
       <ul>
-        {data.map((el, index) => (
-          <li key={el.name}>
-            Name: {el.name}
-            <br />
-            Rotation Period: {el.rotation_period}
-            <br />
-            Orbital Period: {el.orbital_period}
-          </li>
-        ))}
+        {data
+          .filter((e, ind) => ind % 2 !== 1)
+          .map((el) => (
+            <li key={el.name}>
+              Name: {el.name}
+              <br />
+              Rotation Period: {el.rotation_period}
+              <br />
+              Orbital Period: {el.orbital_period}
+            </li>
+          ))}
       </ul>
     </>
   );
