d8a3fda7e60562e77647a15b215ce9ee12a31658
justMaxIt <maximfedor2015@gmail.com>
2020-09-10 12:18:49 +0300

    add buttons planets pagination

--- src/Elements/PlanetsContent.js
+++ src/Elements/PlanetsContent.js
@@ -4,14 +4,15 @@ const api = "https://swapi.dev/api/";
 
 function PlanetsContent(props) {
   const [data, setData] = useState([]);
+  const [numberPlanetsPage, setNumberPlanetsPage] = useState("");
 
   useEffect(() => {
-    fetch(`${api}planets/`)
+    fetch(`${api}planets/${numberPlanetsPage}`)
       .then((res) => res.json())
       .then((res) => setData(res.results))
       .catch((error) => console.error(error));
-  }, []);
-
+  }, [numberPlanetsPage]);
+  console.log(data);
   return (
     <>
       <ul>
@@ -27,6 +28,29 @@ function PlanetsContent(props) {
             </li>
           ))}
       </ul>
+      <button onClick={() => setNumberPlanetsPage("?page=1")}>
+        First page
+      </button>
+      <br /> <br />
+      <button onClick={() => setNumberPlanetsPage("?page=2")}>
+        Second page
+      </button>
+      <br /> <br />
+      <button onClick={() => setNumberPlanetsPage("?page=3")}>
+        Third page
+      </button>
+      <br /> <br />
+      <button onClick={() => setNumberPlanetsPage("?page=4")}>
+        Fourth page
+      </button>
+      <br /> <br />
+      <button onClick={() => setNumberPlanetsPage("?page=5")}>
+        Fifth page
+      </button>
+      <br /> <br />
+      <button onClick={() => setNumberPlanetsPage("?page=6")}>
+        Sixth page
+      </button>
     </>
   );
 }
